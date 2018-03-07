6584fcabcb32fd18315be7b5048e3a411c7e2156
Anthony Bonello <ab@anthonybonello.co.uk>
2018-03-07 15:30:08 +0000

    Display some html

--- run.py
+++ run.py
@@ -2,12 +2,12 @@
 import os
 from flask import Flask
 
-app = Flask(__name__) # Create instance of Flask
+app = Flask(__name__) # Create instance of the Flask class. It is a place holder for the current module
 
 #decorator : 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "<h1>Hello World</h1><h2>This is a Flask Application</h2>"
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), # run the server
