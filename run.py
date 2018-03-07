a974eaa52588228393a64feb1389f6f91f1a8f87
Anthony Bonello <ab@anthonybonello.co.uk>
2018-03-07 15:56:15 +0000

    Using render_template to display html templates.

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 # This is the entry file for the application
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # Create instance of the Flask class. It is a place holder for the current module
 
 #decorator : 
 @app.route('/')
 def hello():
-    return "<h1>Hello World</h1><h2>This is a Flask Application</h2>"
+    # return "<h1>Hello World</h1><h2>This is a Flask Application</h2>"
+    return render_template("index.html")
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), # run the server
